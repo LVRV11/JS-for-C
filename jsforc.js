2c3168453162d8daa72415afe4830b7055b91d4a
LVRV11 <lavrovdmitrii@gmail.com>
2022-01-21 16:44:44 +0200

    first commit

---
+++ jsforc.js
@@ -0,0 +1,44 @@
+// alert (1);
+// ___________________________________________________________
+
+// let drowCats = function (howManyTimes) {
+//    for (let i = 0; i < howManyTimes; i++) {
+    //    console.log (i + " =^.^=");
+//    } 
+// };
+// drowCats (5);
+// 
+// ___________________________________________________________
+
+// let baloon = (15 + 9) *2;
+// alert (baloon);
+// ___________________________________________________________
+
+// let baloon = (15 + 9) *2;
+    // console.log (baloon);
+// ___________________________________________________________
+
+// let script = "Java";
+// console.log (script.length);
+
+// ___________________________________________________________
+
+// let hello = "эЙ кАК деЛа";
+// let lowerString = hello.toLowerCase ();
+// let upString = hello.toUpperCase ();
+// let fitstLetter = upString [0];
+// let secondWord = lowerString.slice (1);
+// let newWord = fitstLetter + secondWord;
+// console.log (newWord);
+
+// ___________________________________________________________
+
+// let age = 11;
+// let accompanied = true;
+// let goToCinema = age > 11 || (age <= 11 && accompanied);
+// console.log (goToCinema);
+
+// ___________________________________________________________
+
+
+
