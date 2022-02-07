966dda6bf24479222ea343fff46127c10126fade
LVRV11 <lavrovdmitrii@gmail.com>
2022-02-07 17:39:11 +0200

    1

--- jsforc.js
+++ jsforc.js
@@ -312,5 +312,30 @@
       
 // }
 // console.log (randomString);
-.
+
+// let input = "javascript is awesome";
+// let output = "";
+
+// for (let i = 0; i < input.length; i++) {
+
+// }
+
+let input = "javascript is awesome";
+let output = "";
+
+for (let i = 0; i < input.length; i++){
+	if(input[i] === "a") {
+      	output += 4;
+     }else if (input[i] === "i"){
+        output += 1; 
+     }else if (input[i] === "e"){
+     		output += 3;
+     }else if (input[i] === "o"){
+     		output += 0;
+     }else{
+     		output += input[i];
+     }
+	}
+  
+    console.log (output);
 
